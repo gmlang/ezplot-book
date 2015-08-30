d3c501f1d3fc2368d21775868ddcc78ca99df830
gmlang <rigor6vigor@gmail.com>
2015-08-29 21:06:55 -0400

    update

--- convert-rmd-to-md.R
+++ convert-rmd-to-md.R
@@ -4,7 +4,7 @@ rm(list=ls())
 library(knitr)
 opts_knit$set(out.format = "markdown")
 opts_chunk$set(comment = "", warning = FALSE, message = FALSE, tidy = FALSE,
-               echo = TRUE, fig.path='images/', fig.width = 8, fig.height = 8, 
+               echo = TRUE, fig.path='images/', fig.width = 7, fig.height = 7, 
                dev = 'png') # don't use fig.align, it forces html code for img
 options(width = 100, scipen = 5, digits = 5)
 
