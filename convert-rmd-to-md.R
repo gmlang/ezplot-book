c3d4b63d96a81015af2340e175874ad0cd2ca28f
gmlang <rigor6vigor@gmail.com>
2015-09-14 21:21:00 -0400

    update

--- convert-rmd-to-md.R
+++ convert-rmd-to-md.R
@@ -32,10 +32,10 @@ rmd_path = file.path(manu_path, "Rmd")
 # file_in = file.path(rmd_path, "03-04-intervalplot.Rmd")
 # knitr::knit(file_in)
 
-file_in = file.path(rmd_path, "03-05-scatterplot.Rmd")
-knitr::knit(file_in)
+# file_in = file.path(rmd_path, "03-05-scatterplot.Rmd")
+# knitr::knit(file_in)
 
-file_in = file.path(rmd_path, "")
+file_in = file.path(rmd_path, "03-06-barplot.Rmd")
 knitr::knit(file_in)
 
 file_in = file.path(rmd_path, "")
