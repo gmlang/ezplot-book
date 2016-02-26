8f901716c9c148f06c3f52f6d19b44e3a9dd1622
gmlang <rigor6vigor@gmail.com>
2016-02-26 14:28:45 -0500

    add dodged and stacked bar chart: 03-06-barplot-2

--- convert-rmd-to-md.R
+++ convert-rmd-to-md.R
@@ -35,10 +35,10 @@ rmd_path = file.path(manu_path, "Rmd")
 # file_in = file.path(rmd_path, "03-05-scatterplot.Rmd")
 # knitr::knit(file_in)
 
-file_in = file.path(rmd_path, "03-06-barplot-1.Rmd")
-knitr::knit(file_in)
+# file_in = file.path(rmd_path, "03-06-barplot-1.Rmd")
+# knitr::knit(file_in)
 
-file_in = file.path(rmd_path, "")
+file_in = file.path(rmd_path, "03-06-barplot-2.Rmd")
 knitr::knit(file_in)
 
 file_in = file.path(rmd_path, "")
