8ef81aadeaa5bc19505fa12b6f27cd42e63457b1
gmlang <rigor6vigor@gmail.com>
2016-03-23 12:16:16 -0400

    update

--- convert-rmd-to-md.R
+++ convert-rmd-to-md.R
@@ -47,8 +47,8 @@ rmd_path = file.path(manu_path, "Rmd")
 # file_in = file.path(rmd_path, "03-07-areaplot.Rmd")
 # knitr::knit(file_in)
 
-file_in = file.path(rmd_path, "03-08-lineplot.Rmd")
-knitr::knit(file_in)
+# file_in = file.path(rmd_path, "03-08-lineplot.Rmd")
+# knitr::knit(file_in)
 
 file_in = file.path(rmd_path, "")
 knitr::knit(file_in)
