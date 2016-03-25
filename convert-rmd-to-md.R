0af040ccf95136cec69bd48020404baaa6961149
gmlang <rigor6vigor@gmail.com>
2016-03-25 15:37:43 -0400

    update 03-10

--- convert-rmd-to-md.R
+++ convert-rmd-to-md.R
@@ -53,8 +53,8 @@ rmd_path = file.path(manu_path, "Rmd")
 # file_in = file.path(rmd_path, "03-09-slopeplot.Rmd")
 # knitr::knit(file_in)
 
-file_in = file.path(rmd_path, "03-10-heatmap.Rmd")
-knitr::knit(file_in)
+# file_in = file.path(rmd_path, "03-10-heatmap.Rmd")
+# knitr::knit(file_in)
 
 file_in = file.path(rmd_path, "")
 knitr::knit(file_in)
